18aa908470ee519be03a4ace8fd8cc2412c6d0c5
RewindNation <70398059+RewindNation-Coding@users.noreply.github.com>
2020-09-11 18:44:16 +0100

    m

---
+++ Rewind-ESX-7/resources/[Rewind]/butcherblip/config.lua
@@ -0,0 +1,21 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 1
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
+Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.Locale                     = 'en'
+
+Config.DriftStations = {
+
+  Mafia = {
+
+    Blip = {
+      Pos     = {x = 955.25, y = -2107.26, z = 31.74},
+      Sprite  = 85,
+      Display = 4,
+      Scale   = 0.7,
+      Colour  = 29,
+    },
+  },
+
+}
