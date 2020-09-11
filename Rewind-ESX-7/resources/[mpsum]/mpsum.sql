37e23427e849b0d5da9bf07e46c3eda41e079649
RewindNation <70398059+RewindNation-Coding@users.noreply.github.com>
2020-09-11 19:05:06 +0100

    noti

---
+++ Rewind-ESX-7/resources/[mpsum]/mpsum.sql
@@ -0,0 +1,23 @@
+USE `essentialmode`;
+
+INSERT INTO `vehicle_categories` (name, label) VALUES
+	('mpsum','MP Summer')
+;
+
+INSERT INTO `vehicles` (name, model, price, category) VALUES
+	('Openwheel I','openwheel1',1250000,'mpsum'),
+	('Openwheel II','openwheel2',1350000,'mpsum'),
+	('Coquette IV','coquette4',47000,'mpsum'),
+	('Tigon','tigon',72000,'mpsum'),
+	('Penumbra II','penumbra2',28000,'mpsum'),
+	('Landstalker II','landstalker2',110000,'mpsum'),
+	('Yosemite III','yosemite3',200000,'mpsum'),
+	('Club','club',45000,'mpsum'),
+	('Gauntlet V','gauntlet5',65000,'mpsum'),
+	('Dukes III','dukes3',35000,'mpsum'),
+	('Youga III','youga3',47000,'mpsum'),
+	('Glendale II','glendale2',32000,'mpsum'),
+	('Seminole II','seminole2',45000,'mpsum'),
+	('Peyote III','peyote3',48000,'mpsum'),
+	('Manana II','manana2',175000,'mpsum')
+;
